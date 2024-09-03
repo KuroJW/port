500623546627d9b93c4daa41743d30b10542b4ff
KuroJW <126410409+KuroJW@users.noreply.github.com>
2024-09-03 10:17:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function Dark()
+{
+    document.getElementById('body').classList.add('Background-color');
+}
+
+function Light() 
+{
+    document.getElementById('body').classList.remove('Background-color');
+    document.getElementById('div-login').classList.remove('login-dark');
+}
